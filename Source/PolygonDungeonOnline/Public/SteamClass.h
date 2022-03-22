540274b202ccf307278d7aae10d33690acbea931
dchepurkin <dchepurkin@mail.ru>
2022-03-22 22:08:18 +0300

    22.03.2022 22:06 Дома
    Реализован удаленный сервер через Steam

---
+++ Source/PolygonDungeonOnline/Public/SteamClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class POLYGONDUNGEONONLINE_API SteamClass
+{
+public:
+	SteamClass();
+	~SteamClass();
+};
